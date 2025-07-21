b59d37ed29022a52ffd4646774c4773c740f9b5b
takkun765 <108818255+takkun765@users.noreply.github.com>
2025-07-21 14:23:53 +0900

    Update comment.js

--- pages/api/comment.js
+++ pages/api/comment.js
@@ -4,6 +4,6 @@ const prisma = new PrismaClient()
 
 export async function GET() {
   // Postテーブルから全件取得
-  const todos: Post[] = await prisma.Post.findMany()
+  const posts: Post[] = await prisma.post.findMany()
   return Response.json(posts)
 }
