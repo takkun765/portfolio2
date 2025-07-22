1faf2f78a7e75e2d44c1ef3a837f47e5e4f0e299
takkun765 <108818255+takkun765@users.noreply.github.com>
2025-07-22 14:32:12 +0900

    test

--- pages/api/comment.js
+++ pages/api/comment.js
@@ -1,9 +1,9 @@
-import { PrismaClient, Post } from '@prisma/client'
+// import { PrismaClient, Post } from '@prisma/client'
 
-const prisma = new PrismaClient()
+// const prisma = new PrismaClient()
 
-export async function GET() {
-  // Postテーブルから全件取得
-  const posts: Post[] = await prisma.post.findMany()
-  return Response.json(posts)
-}
+// export async function GET() {
+//   // Postテーブルから全件取得
+//   const posts: Post[] = await prisma.Post.findMany()
+//   return Response.json(posts)
+// }
--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
         <main className={styles.main}>
           <Image
             className={styles.logo}
-            src="/next.svg"
+            src="/Access_db01.PNG"
             alt="Next.js logo"
             width={180}
             height={38}
